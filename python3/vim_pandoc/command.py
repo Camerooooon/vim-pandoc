7157cccdfc932ec01b9d5d56a8916825058e7832
Camerooooon <cjpuffnstuff@gmail.com>
2023-07-16 15:31:48 -0700

    Try setting buffer size to 0

--- python3/vim_pandoc/command.py
+++ python3/vim_pandoc/command.py
@@ -165,9 +165,9 @@ class PandocCommand(object):
                     vim.command(wnr + "wincmd c")
                     vim.command(wnr + "put='Running pandoc...\n'")
 
-                vim.command("botright 7new pandoc-execute")
+                vim.command("botright 0new pandoc-execute")
                 vim.command("setlocal buftype=nofile")
-                vim.command("setlocal bufhidden=hidden")
+                vim.command("setlocal bufhidden=wipe")
                 vim.command("setlocal nobuflisted")
                 vim.command("map <buffer> q <Esc>:close<Enter>")
                 vim.command("call termopen(" + \
