8b6dcfd6947e00ac96a75e5d1f0c78e9b56fa538
Camerooooon <cjpuffnstuff@gmail.com>
2023-07-16 15:36:19 -0700

    Don't try to hide

--- python3/vim_pandoc/command.py
+++ python3/vim_pandoc/command.py
@@ -179,7 +179,6 @@ class PandocCommand(object):
                 vim.command("file pandoc-execute")
                 vim.command("normal G")
                 vim.command("wincmd p")
-                vim.command("hide pandoc-execute")
 
             # for vim versions with clientserver support
             elif vim.eval("has('clientserver')") == '1' and \
